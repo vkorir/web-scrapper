1a90b5e594003b5beeb50675c2b9d279d0b192d9
Victor Korir <victor@Vics-Mac.local>
2018-06-04 23:15:11 -0700

    a very dummy scrapper

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const request = require("request-promise");
+const cheerio = require("cheerio");
+
+const app = express();
+const port = process.env.NODE_END || 5000;
+
+const getOptions = uri => {
+  return {
+    uri,
+    json: true,
+    transform: body => {
+      return cheerio.load(body);
+    }
+  };
+};
+
+app.get("/scrapper", async (req, res) => {
+    // const options = getOptions("https://www.google.com")
+    // const data = await request(options)
+    // if (data.errors) {
+    //     res.send("Ouch! Found error :(")
+    // }
+    // res.send(data)
+    res.send( {
+        "div": {
+            "oops!": "Bleh bleh",
+            "h4": "Coming soon! :)"
+        }
+
+    })
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
